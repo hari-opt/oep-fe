a0e58b931dbf2a60c45fd3030252e603a3410543
2017572-L\Ram Mohan <rammohan@chrysalis.world>
2022-10-06 14:32:41 +0530

    fe

---
+++ src/config/colors.js
@@ -0,0 +1,12 @@
+export default {
+  primary: "#ec4608",
+  secondary: "#222",
+  bg: "#ecedef",
+  black: "#000",
+  white: "#fff",
+  textlight: "#ccc",
+  medium: "#6e6969",
+  light: "#f8f4f4",
+  danger: "#ff5252",
+  dark: "#2f2f2f",
+};
