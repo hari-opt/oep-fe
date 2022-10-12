daa1ac7a41fb1524ecb6babb70ba252c406e6822
2017572-L\Ram Mohan <rammohan@chrysalis.world>
2022-10-12 13:00:56 +0530

    navbar

--- src/components/NavItems/NavItems.js
+++ src/components/NavItems/NavItems.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
+import AccessAll from "../NavTabs/AccessAll/AccessAll";
+import MyLinks from "../NavTabs/MyLinks/MyLinks";
+import Referral from "../NavTabs/Referral/Referral";
+import UpdateProfile from "../NavTabs/UpdateProfile/UpdateProfile";
 import "./NavItems.css";
 
 function NavItems() {
@@ -13,7 +19,7 @@ function NavItems() {
           <li className="nav-li">Referral</li>
         </ul>
       </div> */}
-      <Nav
+      {/* <Nav
         className="nav-container"
         justify
         variant="tabs"
@@ -28,7 +34,26 @@ function NavItems() {
         <Nav.Item>
           <Nav.Link eventKey="link-3">Referral</Nav.Link>
         </Nav.Item>
-      </Nav>
+      </Nav>*/}
+      <Tabs
+        defaultActiveKey="access-all"
+        id="justify-tab-example"
+        className="mb-3 nav-container nav-tab-container"
+        justify
+      >
+        <Tab eventKey="access-all" title="Access All">
+          <AccessAll />
+        </Tab>
+        <Tab eventKey="update-profile" title="Update Profile">
+          <UpdateProfile />
+        </Tab>
+        <Tab eventKey="referral" title="Referral">
+          <Referral />
+        </Tab>
+        <Tab eventKey="my-links" title="My Links">
+          <MyLinks />
+        </Tab>
+      </Tabs>
     </>
   );
 }
---
+++ src/components/NavTabs/MyLinks/MyLinks.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyLinks(props) {
+  return (
+    <div>
+      <h1>My links</h1>
+    </div>
+  );
+}
+
+export default MyLinks;
